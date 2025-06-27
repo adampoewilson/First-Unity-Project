a5697b750b9c56ae6b72cd691e79ca4edf1d9ca0
adampoewilson <adam62385@gmail.com>
2025-06-26 22:43:17 -0400

    added code to destroy pipes

--- Assets/PipeMovement.cs
+++ Assets/PipeMovement.cs
@@ -7,16 +7,27 @@ public class PipeMovement : MonoBehaviour
 
     public float moveSpeed = 2;
 
+    public float deadZone = -45;
+
     // Start is called before the first frame update
-    void Start()
+  void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    { // Time.deltaTime means movement happens at same speed
-      // No matter frame rate
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+  // Update is called once per frame
+  void Update()
+  { // Time.deltaTime means movement happens at same speed
+    // No matter frame rate
+    transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+    // Destroy some pipes
+
+    if (transform.position.x < deadZone)
+    {
+
+      Destroy(gameObject);
+
+    }
     }
 }
