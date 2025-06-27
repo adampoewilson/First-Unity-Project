f7c085186ba051f8298ed1f2008c8be2356f530a
adampoewilson <adam62385@gmail.com>
2025-06-26 20:52:06 -0400

    mother fucker

--- Assets/PipeSpawn.cs
+++ Assets/PipeSpawn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class PipeSpawn : MonoBehaviour
@@ -10,6 +11,8 @@ public class PipeSpawn : MonoBehaviour
     public float spawnRate = 1.5f;
 
     public float timer = 0;
+
+    public float heightOffset = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -41,9 +44,9 @@ public class PipeSpawn : MonoBehaviour
 
     void spawnPipe()
     {
-
-        Instantiate(pipe, transform.position, transform.rotation);
-
+        
+        
+        
     }
 }
 
